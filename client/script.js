171ffd3b475ac8fe74e2bc075cb666b977d74eeb
kkapi <hZG2fik_j8@mail.ru>
2023-05-12 14:09:48 +0300

    add placemark

--- client/script.js
+++ client/script.js
@@ -6,11 +6,21 @@ function init () {
     let map = new ymaps.Map('map', {
         // При инициализации карты обязательно нужно указать
         // её центр и коэффициент масштабирования.
-        center: [59.93657755549354,30.305595145507816], // Спб
-        zoom: 11
+        center: [59.94892727754434,30.318982137709053], // Спб
+        zoom: 12
     }, {
         searchControlProvider: 'yandex#search'
     });
+
+    let placemark = new ymaps.Placemark([59.94892727754434,30.318982137709053], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'https://cdn-icons-png.flaticon.com/512/3178/3178117.png',
+        // iconImageHref: 'https://cdn-icons-png.flaticon.com/512/7902/7902219.png',
+        // iconImageHref: 'https://cdn-icons-png.flaticon.com/512/63/63845.png',
+        // iconImageHref: 'https://cdn-icons-png.flaticon.com/512/3178/3178257.png',
+        iconImageSize: [30, 30],
+        iconImageOffset: [-15, -15]
+    })
 
     map.controls.remove('geolocationControl'); // удаляем геолокацию
     map.controls.remove('searchControl'); // удаляем поиск
@@ -20,4 +30,6 @@ function init () {
     map.controls.remove('zoomControl'); // удаляем контрол зуммирования
     map.controls.remove('rulerControl'); // удаляем контрол правил
     // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+
+    map.geoObjects.add(placemark);
 }
