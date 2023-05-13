aa27c535c22b595b995e754c7608cf8c50eae511
kkapi <hZG2fik_j8@mail.ru>
2023-05-13 12:48:13 +0300

    map routing

--- client/script.js
+++ client/script.js
@@ -4,9 +4,8 @@ function init() {
     let map = new ymaps.Map('map', {
         center: SPB_CENTER, // SPB_CENTER
         zoom: 12,
+        controls: []
     }, {});
-
-    removeControls(map);
 }
 
 const coordinatesForm = document.querySelector('#coordinates-form');
@@ -20,7 +19,7 @@ function formHandler(event) {
     event.preventDefault();
 
     if (radio.checked) {
-        getAreaPoint();      
+        getAreaPoint();
     } else {
         getClosestPoint();
     }
@@ -34,7 +33,9 @@ async function getClosestPoint() {
 
     await fetch(`${SERVER_HOST}/getWiFiNear?lon=${longitudeValue}&lat=${latitudeValue}`, {})
         .then(response => response.json())
-        .then(json => closestPoint = json)
+        .then(json => closestPoint = json);
+
+    console.log(closestPoint.coordinates);
 
     addPlacemarks([latitudeValue, longitudeValue], [closestPoint])
 }
@@ -53,11 +54,11 @@ async function getAreaPoint() {
         .then(json => points = json)
 
     // appendPoints(points)
-    
+
     addPlacemarks([latitudeValue, longitudeValue], points)
 }
 
-function appendPoints (points) {
+function appendPoints(points) {
     let list = document.createElement('ul');
 
     let div = document.querySelector('#points')
@@ -75,24 +76,14 @@ function appendPoints (points) {
     div.append(list)
 }
 
-function removeControls(map) {
-    map.controls.remove('geolocationControl'); // удаляем геолокацию
-    map.controls.remove('searchControl'); // удаляем поиск
-    map.controls.remove('trafficControl'); // удаляем контроль трафика
-    map.controls.remove('typeSelector'); // удаляем тип
-    map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-    map.controls.remove('zoomControl'); // удаляем контрол зуммирования
-    map.controls.remove('rulerControl'); // удаляем контрол правил
-    // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
-}
-
 function addPlacemarks(center, points) {
     let mapDiv = document.querySelector('#map');
     mapDiv.innerHTML = '';
 
     let map = new ymaps.Map('map', {
         center: center,
-        zoom: 16
+        zoom: 16,
+        controls: []
     });
 
     let centerPlacemark = new ymaps.Placemark(center, {
@@ -106,19 +97,19 @@ function addPlacemarks(center, points) {
 
     map.geoObjects.add(centerPlacemark);
 
-    for (let point in points) {        
+    for (let point in points) {
         let placemark = new ymaps.Placemark(points[point].coordinates, {
             balloonContentHeader: `${points[point].name_wifi}`,
-            balloonContentBody: `Адресc:${points[point].address}, 
-                                Район:${points[point].district}, 
-                                Покрытие:${points[point].coverage}м, 
-                                Статус:${points[point].status}`,
-            balloonContentFooter: `<button onclick="test(${points[point].coordinates[0]},
-                                                         ${points[point].coordinates[1]},
-                                                         ${center[0]},
-                                                         ${center[1]})">
-                                        Кнопка
-                                    </button>`        
+            balloonContentBody: `Адрес: ${points[point].address}, 
+                                Район: ${points[point].district}, 
+                                Покрытие: ${points[point].coverage}м, 
+                                Статус: ${points[point].status}`,
+            balloonContentFooter: `<button onclick="buildRoute(${points[point].coordinates[0]},
+                                    ${points[point].coordinates[1]},
+                                    ${center[0]},
+                                    ${center[1]})">
+                                    Построить маршрут до точки
+                                </button>`
         }, {
             iconLayout: 'default#image',
             iconImageHref: 'https://cdn-icons-png.flaticon.com/512/3898/3898607.png',
@@ -128,25 +119,50 @@ function addPlacemarks(center, points) {
 
         map.geoObjects.add(placemark);
     }
-
-    removeControls(map);
 }
 
-function test(a,b,c,d) {
-    console.log(a,b,c,d)
+function buildRoute(latitudeB, longitudeB, latitudeA, longitudeA) {
+    console.log(latitudeA, longitudeA, latitudeB, longitudeB)
+    console.log(typeof(latitudeA), typeof(longitudeA), typeof(latitudeB), typeof(longitudeB))
+
+    let pointA = [latitudeA, longitudeA]
+    let pointB = [latitudeB, longitudeB]
+
+    let mapDiv = document.querySelector('#map');
+    mapDiv.innerHTML = '';
+
+    let map = new ymaps.Map('map', {
+        center: pointA,
+        zoom: 16,
+        controls: []
+    });
+
+    let multiRoute = new ymaps.multiRouter.MultiRoute({
+        referencePoints: [
+            pointA,
+            pointB
+        ],
+        params: {
+            routingMode: 'pedestrian'
+        }
+    }, {
+        boundsAutoApply: true
+    });
+
+    map.geoObjects.add(multiRoute);
 }
 
 const radio = document.querySelector('#radio')
 const submitButton = document.querySelector('#submit-btn')
 
-radio.addEventListener('change', function() {
-    if (this.checked) {      
-      radiusInput.disabled = false;
-      radiusInput.value = '';
-      submitButton.innerText = 'Найти все Wi-Fi точки в радиусе';
+radio.addEventListener('change', function () {
+    if (this.checked) {
+        radiusInput.disabled = false;
+        // radiusInput.value = '';
+        submitButton.innerText = 'Найти все Wi-Fi точки в радиусе';
     } else {
-      radiusInput.disabled = true;
-      radiusInput.value = '';
-      submitButton.innerText = 'Найти ближашую Wi-Fi точку';
+        radiusInput.disabled = true;
+        // radiusInput.value = '';
+        submitButton.innerText = 'Найти ближашую Wi-Fi точку';
     }
-  });
+});
